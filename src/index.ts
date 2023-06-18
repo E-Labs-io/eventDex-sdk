1a8358ed3f28db81edd6fe50fc58171080995412
E-Labs-io <92272091+E-Labs-io@users.noreply.github.com>
2023-06-18 17:05:57 +0200

    adding commets

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,8 @@ import EventDexAPI from "./api/eventDexAPI";
 // Create an instance of the EventDexAPI class
 const sdkInstance = (api) => new EventDexAPI(api);
 
+/**
+ * 
+ */
 // Export the sdkInstance for users to interact with the SDK
 export default sdkInstance;

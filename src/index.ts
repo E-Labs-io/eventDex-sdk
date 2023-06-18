2ffd554ce340c22770b65bf28ed205b687df7dbb
E-Labs-io <92272091+E-Labs-io@users.noreply.github.com>
2023-06-18 22:24:31 +0200

    updates

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,10 @@
 import EventDexAPI from "./api/eventDexAPI";
 
 // Create an instance of the EventDexAPI class
-const sdkInstance = (api) => new EventDexAPI(api);
+const sdkInstance = (api: string) => new EventDexAPI(api);
 
 /**
- * 
+ *
  */
 // Export the sdkInstance for users to interact with the SDK
 export default sdkInstance;
